3d2792f64092158a05d00fc3c4cf34a9b49edc57
Ronan <33204767+Ronan7x@users.noreply.github.com>
2022-11-18 06:33:22 -0800

    Added commands

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 const { Client, GatewayIntentBits } = require('discord.js')
-const client = new Client({
+
+ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        // ...
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildBans
+        
+        
     ]
 })
+
+const prefix = '-';
 
 client.once('ready', () => {
     console.log('BasedBot is online!');
 });
 
+client.on('messageCreate', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
 
-client.login("MTA0MDg4MjA3NTE3MzEzODUxNQ.GE5m32.ENnp9cAMXdaIzzIpDiThcfaTopKl30_VoRPD9Y");
+    const  args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'ping'){
+        message.channel.send('pong!');
+    } else if (command == 'youtube'){
+        message.channel.send('https://www.youtube.com/@rascal7x');
+
+    }
+    
+});
+
+
+
+
+
